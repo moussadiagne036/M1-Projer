a07a3f0d05898e1e3c8e6285d91949744140b21e
moussadiagne036 <82718253+moussadiagne036@users.noreply.github.com>
2025-05-01 21:27:00 +0000

    premier commit

---
+++ src/main/java/sn/unchk/Projet_Bibliotheque/ProjetBibliothequeApplication.java
@@ -0,0 +1,13 @@
+package sn.unchk.Projet_Bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBibliothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetBibliothequeApplication.class, args);
+	}
+
+}
